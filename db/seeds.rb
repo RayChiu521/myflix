57eca767e68ee2082512f667a2f1871be73ce260
Ray <huihong521@gmail.com>
2013-12-11 20:35:48 +0800

    1. Create the categories table. 2. Create the category model and build 1:M relations with videos. 3. Create categories in seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Video.create!(title: 'MONK', description: "http://en.wikipedia.org/wiki/Monk_(TV_series)", small_cover_url: 'monk.jpg', large_cover_url: 'monk_large.jpg')
-Video.create!(title: 'futurama', description: "http://en.wikipedia.org/wiki/Futurama", small_cover_url: 'futurama.jpg', large_cover_url: 'futurama.jpg')
-Video.create!(title: 'south park', description: "http://en.wikipedia.org/wiki/South_Park", small_cover_url: 'south_park.jpg', large_cover_url: 'south_park.jpg')
-Video.create!(title: 'family guy', description: "http://en.wikipedia.org/wiki/Family_Guy", small_cover_url: 'family_guy.jpg', large_cover_url: 'family_guy.jpg')
+category1 = Category.create!(title: 'Action')
+category2 = Category.create!(title: 'Comedy')
+
+category1.videos.create!(title: 'MONK', description: "http://en.wikipedia.org/wiki/Monk_(TV_series)", small_cover_url: 'monk.jpg', large_cover_url: 'monk_large.jpg')
+category1.videos.create!(title: 'futurama', description: "http://en.wikipedia.org/wiki/Futurama", small_cover_url: 'futurama.jpg', large_cover_url: 'futurama.jpg')
+category2.videos.create!(title: 'south park', description: "http://en.wikipedia.org/wiki/South_Park", small_cover_url: 'south_park.jpg', large_cover_url: 'south_park.jpg')
+category2.videos.create!(title: 'family guy', description: "http://en.wikipedia.org/wiki/Family_Guy", small_cover_url: 'family_guy.jpg', large_cover_url: 'family_guy.jpg')
