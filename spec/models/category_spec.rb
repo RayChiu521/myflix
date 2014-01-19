4002847dd5e5e439edb8fb98f976e91004033b87
Ray <huihong521@gmail.com>
2014-01-19 13:48:26 +0800

    Use shoulda_matchers.

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,15 +2,6 @@ require 'spec_helper'
 
 describe Category do
 
-  it "saves itself" do
-    funny = Category.create!(title: "funny")
-    expect(Category.first).to eq(funny)
-  end
+  it { should have_many(:videos) }
 
-  it "has many videos" do
-    funny = Category.create!(title: "funny")
-    friends = Video.create!(title: "friends", description: "a funny television sitcom.", category: funny)
-    south_park = Video.create!(title: "south park", description: "a funny cartoon.", category: funny)
-    expect(funny.videos).to eq([friends, south_park])
-  end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -2,24 +2,8 @@ require 'spec_helper'
 
 describe Video do
 
-  it "saves itself" do
-    video = Video.create!(title: "video", description: "testing video")
-    expect(Video.first).to eq(video)
-  end
+  it { should belong_to(:category) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
 
-  it "belongs to category" do
-    funny = Category.create!(title: "funny")
-    friends = Video.create!(title: "friends", description: "a funny television sitcom.", category: funny)
-    expect(friends.category).to eq(funny)
-  end
-
-  it "does not save a video without a title" do
-    video = Video.create(description: "description")
-    expect(Video.all.include?(video)).to eq(false)
-  end
-
-  it "does not save a video without a description" do
-    video = Video.create(title: "friends")
-    expect(Video.all.include?(video)).to eq(false)
-  end
 end
