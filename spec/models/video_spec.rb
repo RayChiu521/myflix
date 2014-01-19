a9f2d43f9b3b9948787b830f59fa23d8406d4085
Ray <huihong521@gmail.com>
2014-01-19 13:36:16 +0800

    Testing the presence validation of video.

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -12,4 +12,14 @@ describe Video do
     friends = Video.create!(title: "friends", description: "a funny television sitcom.", category: funny)
     expect(friends.category).to eq(funny)
   end
+
+  it "does not save a video without a title" do
+    video = Video.create(description: "description")
+    expect(Video.all.include?(video)).to eq(false)
+  end
+
+  it "does not save a video without a description" do
+    video = Video.create(title: "friends")
+    expect(Video.all.include?(video)).to eq(false)
+  end
 end
