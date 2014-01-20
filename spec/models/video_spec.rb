500cdb8229bb714eed4d4fe92512193485853ebd
Ray <huihong521@gmail.com>
2014-01-20 23:34:55 +0800

    1. Add the search_by_title method to the Video model. 2. Testing the search_by_title method.

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -6,4 +6,39 @@ describe Video do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
 
+
+  describe "search_by_title" do
+
+    it "returns an empty array if there is no match" do
+      friends = Video.create(title: "Friends", description: "a tv show.")
+      pianist = Video.create(title: "Pianist", description: "a movie.")
+      expect(Video.search_by_title("Red")).to eq([])
+    end
+
+    it "returns and array of one video for an exact match" do
+      friends = Video.create(title: "Friends", description: "a tv show.")
+      pianist = Video.create(title: "Pianist", description: "a movie.")
+      expect(Video.search_by_title("Friends")).to eq([friends])
+    end
+
+    it "returns and array of one video for a partial match" do
+      friends = Video.create(title: "Friends", description: "a tv show.")
+      pianist = Video.create(title: "Pianist", description: "a movie.")
+      expect(Video.search_by_title("Fri")).to eq([friends])
+    end
+
+    it "returns and array of all matchs ordered by created_at" do
+      friends = Video.create(title: "Friends", description: "a tv show.", created_at: 1.day.ago)
+      pianist = Video.create(title: "Pianist", description: "a movie.")
+      expect(Video.search_by_title("i")).to eq([pianist, friends])
+    end
+
+    it "returns an empty array for a search with an empty string" do
+      friends = Video.create(title: "Friends", description: "a tv show.", created_at: 1.day.ago)
+      pianist = Video.create(title: "Pianist", description: "a movie.")
+      expect(Video.search_by_title("")).to eq([])
+    end
+
+  end
+
 end
