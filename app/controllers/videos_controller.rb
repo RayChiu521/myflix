58836578ae7d6da741cac1e52dcf111cb96f1127
Ray <huihong521@gmail.com>
2014-01-21 22:19:21 +0800

    Implement search in the UI.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -6,6 +6,11 @@ class VideosController < ApplicationController
 	def show
 	end
 
+  def search
+    redirect_to root_path and return if params[:search_term].blank?
+    @videos = Video.search_by_title(params[:search_term])
+  end
+
 
 	private
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
   get 'genre', to: 'categories#index'
 
-  resources :videos, only: [:show]
+  resources :videos, only: [:show] do
+    collection do
+      post 'search', to: 'videos#search'
+    end
+  end
 
   root 'home#index'
 end
