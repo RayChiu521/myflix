5c0b5711378daf66365afa533373c6817769f44b
Ray <huihong521@gmail.com>
2014-01-25 09:15:06 +0800

    Disabled the validations of user password secure.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation, :full_name)
+      params.require(:user).permit(:email, :password, :full_name)
     end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-  has_secure_password validations: true
+  has_secure_password validations: false
 
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, on: :create
