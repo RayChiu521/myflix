ec7fb3c31f41b0eb3f9c335799b65aa3168a5274
Ray <huihong521@gmail.com>
2014-01-25 09:05:46 +0800

    Added the timestamps field to users.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140123140928) do
+ActiveRecord::Schema.define(version: 20140125010408) do
 
   create_table "categories", force: true do |t|
     t.string   "title"
@@ -20,9 +20,11 @@ ActiveRecord::Schema.define(version: 20140123140928) do
   end
 
   create_table "users", force: true do |t|
-    t.string "email"
-    t.string "password_digest"
-    t.string "full_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "full_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "videos", force: true do |t|
