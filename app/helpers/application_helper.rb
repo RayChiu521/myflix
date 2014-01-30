75425326fd5a3f186d872e921851effd38a78ac7
Ray <huihong521@gmail.com>
2014-01-30 16:32:36 +0800

    Added the video review feature and test spec.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+  def options_for_rating_star
+    options_for_select((1..5).to_a.reverse.map { |number| [ pluralize(number, "Start"), number] })
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,5 @@ class Review < ActiveRecord::Base
   belongs_to :video
   belongs_to :creator, class_name: "User", foreign_key: "user_id"
 
+  validates_presence_of :content, :rating
 end
