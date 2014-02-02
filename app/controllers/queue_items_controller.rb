81427974133e71dff8c33dec6bcaf3653cd4d2a1
Ray <huihong521@gmail.com>
2014-02-02 22:34:04 +0800

    Moved codes from controller to model.

--- app/controllers/queue_items_controller.rb
+++ app/controllers/queue_items_controller.rb
@@ -14,7 +14,7 @@ class QueueItemsController < ApplicationController
     queue_item = QueueItem.find(params[:id])
     if queue_item.creator == current_user
       queue_item.destroy
-      normalize_queue_item_positions
+      current_user.normalize_queue_item_positions
     end
     redirect_to my_queue_path
   end
@@ -22,7 +22,7 @@ class QueueItemsController < ApplicationController
   def update_queue
     begin
       update_queue_items
-      normalize_queue_item_positions
+      current_user.normalize_queue_item_positions
     rescue ActiveRecord::RecordInvalid
       flash[:alert] = "Invalid position numbers."
     rescue Exception => e
@@ -56,12 +56,6 @@ private
         end
         raise "Duplicated position numbers." if current_user.queue_items.select("position").group("position").having("count(*) > 1").length > 0
       end
-    end
-  end
-
-  def normalize_queue_item_positions
-    current_user.queue_items.each_with_index do |queue_item, index|
-      queue_item.update_attributes(position: (index + 1))
     end
   end
 end
