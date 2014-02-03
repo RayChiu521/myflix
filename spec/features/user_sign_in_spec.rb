ce5c072bc72a50ebae19c7ce7f171b7c297bb784
Ray <huihong521@gmail.com>
2014-02-03 13:56:15 +0800

    Refactored by solution video.

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -1,14 +1,14 @@
 require "spec_helper"
 
 feature "User sign in" do
-  given(:henryk) { Fabricate(:user, password: "password") }
+  given(:henryk) { Fabricate(:user) }
 
   scenario "Signing in with correct credentials" do
     visit sign_in_path
     fill_in "Email Address", with: henryk.email
     fill_in "Password", with: henryk.password
     click_on "Sign in"
-    expect(page).to have_content "You've logged in."
+    page.should have_content henryk.full_name
   end
 
   scenario "Signing in with incorrect credentials" do
@@ -16,6 +16,6 @@ feature "User sign in" do
     fill_in "Email Address", with: henryk.email
     fill_in "Password", with: "#{henryk.password}abc"
     click_on "Sign in"
-    expect(page).to have_content "There's something wrong with your email or password."
+    page.should have_content "There's something wrong with your email or password."
   end
 end
