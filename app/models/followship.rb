c1ea2ccf1019257439238e90f968d234bfe23f7f
Ray <huihong521@gmail.com>
2014-02-05 19:40:38 +0800

    Removed useless foreign_keys.

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,6 +1,6 @@
 class Followship < ActiveRecord::Base
 
-  belongs_to :leader, class_name: "User", foreign_key: "leader_id"
-  belongs_to :follower, class_name: "User", foreign_key: "follower_id"
+  belongs_to :leader, class_name: "User"
+  belongs_to :follower, class_name: "User"
 
 end
