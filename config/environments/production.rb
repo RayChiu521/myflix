fd0106adafdf8a78aa2ac44ad77f6f59f72091c6
Ray <huihong521@gmail.com>
2014-02-12 20:40:31 +0800

    Commented out mail's from on the production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_options = { from: ENV["EMAIL_ACCOUNT"] }
+  # config.action_mailer.default_options = { from: ENV["EMAIL_ACCOUNT"] }
   config.action_mailer.default_url_options = { host: 'http://my-flix.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
