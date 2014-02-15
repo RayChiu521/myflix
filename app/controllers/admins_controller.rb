34dff6f342bc5cb40a32054dba08eb85c299d13b
Ray <huihong521@gmail.com>
2014-02-15 20:48:50 +0800

    Added a base controller for admin actor.

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,9 @@
+class AdminsController < AuthenticatedController
+  before_action :require_admin
+
+private
+
+  def require_admin
+    redirect_to root_path, alert: "You do not have access to that area!" unless current_uesr.admin?
+  end
+end
