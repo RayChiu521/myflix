783211bba33e84b111f5bd4bf2e920d31a0a8bb2
Ray <huihong521@gmail.com>
2014-02-16 16:53:18 +0800

    Added the watch video feature.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140216033717) do
+ActiveRecord::Schema.define(version: 20140216084808) do
 
   create_table "categories", force: true do |t|
     t.string   "title"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 20140216033717) do
     t.integer  "category_id"
     t.string   "small_cover"
     t.string   "large_cover"
+    t.string   "video_url"
   end
 
   add_index "videos", ["category_id"], name: "index_videos_on_category_id"
