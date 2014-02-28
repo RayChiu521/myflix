ebc56f9f9fde0d2439899a4e29c168ed21db5372
Ray <huihong521@gmail.com>
2014-02-28 23:13:43 +0800

    Refactor by solution.

--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -5,54 +5,106 @@ feature "User registration", { js: true, vcr: true } do
     visit register_path
   end
 
-  scenario "user inputs valid personal data and valid credit card" do
-    fill_in_personal_data("phoebe@example")
-    fill_in_credit_card(card: "4242424242424242")
+  scenario "with valid personal data and valid credit card" do
+    fill_in_valid_personal_data
+    fill_in_valid_credit_card
 
     click_on "Sign Up"
 
     expect_content("User was created")
   end
 
-  scenario "user inputs valid personal data and declined card" do
-    fill_in_personal_data("phoebe@example")
-    fill_in_credit_card(card: "4000000000000002")
+  scenario "with valid personal data and invalid credit card" do
+    fill_in_valid_personal_data
+    fill_in_invalid_personal_data
 
     click_on "Sign Up"
 
-    expect_content("Your card was declined.")
+    expect_content("This card number looks invalid")
   end
 
-  scenario "user inputs valid personal data and incorrect card number" do
-    fill_in_personal_data("phoebe@example")
-    fill_in_credit_card(card: "4242424242424241")
+  scenario "with valid personal data and declined card" do
+    fill_in_valid_personal_data
+    fill_in_declined_credit_card
 
     click_on "Sign Up"
 
-    expect_content("Your card number is incorrect")
+    expect_content("Your card was declined")
   end
 
-  scenario "user inputs valid personal data and invalid security code" do
-    fill_in_personal_data("phoebe@example")
-    fill_in_credit_card(card: "4242424242424242", security_code: "99")
+  scenario "with valid personal data and invalid security code" do
+    fill_in_valid_personal_data
+    fill_in_invalid_security_code
 
     click_on "Sign Up"
 
     expect_content("Your card's security code is invalid")
   end
 
-  scenario "user inputs invalid personal data" do
-    fill_in_personal_data("")
-    fill_in_credit_card(card: "4242424242424242")
+  scenario "with invalid personal data and valid card" do
+    fill_in_personal_data
+    fill_in_valid_credit_card
 
     click_on "Sign Up"
 
     expect_content("Please fix the error below")
   end
 
-  def fill_in_personal_data(email)
-    fill_in "Email Address", with: email
-    fill_in "Password", with: "password"
-    fill_in "Full name", with: "Phoebe Buffay"
+  scenario "with invalid personal data and invalid card" do
+    fill_in_invalid_personal_data
+    fill_in_invalid_credit_card
+
+    click_on "Sign Up"
+
+    expect_content("This card number looks invalid")
+  end
+
+  scenario "with invalid personal data and declined card" do
+    fill_in_invalid_personal_data
+    fill_in_declined_credit_card
+
+    click_on "Sign Up"
+
+    expect_content("Please fix the error below")
+  end
+
+  scenario "with invalid personal data and invalid security code" do
+    fill_in_invalid_personal_data
+    fill_in_invalid_security_code
+
+    click_on "Sign Up"
+
+    expect_content("Your card's security code is invalid")
+  end
+
+
+  def fill_in_valid_personal_data
+    fill_in_personal_data(email: "phoebe@example.com", password: "password", full_name: "Phoebe Buffay")
+  end
+
+  def fill_in_invalid_personal_data
+    fill_in_personal_data
+  end
+
+  def fill_in_personal_data(options = {})
+    fill_in "Email Address", with: options[:email] || ""
+    fill_in "Password", with: options[:password] || ""
+    fill_in "Full name", with: options[:full_name] || ""
+  end
+
+  def fill_in_valid_credit_card
+    fill_in_credit_card(card: "4242424242424242")
+  end
+
+  def fill_in_invalid_credit_card
+    fill_in_credit_card(card: "123")
+  end
+
+  def fill_in_declined_credit_card
+    fill_in_credit_card(card: "4000000000000002")
+  end
+
+  def fill_in_invalid_security_code
+    fill_in_credit_card(card: "4242424242424242", security_code: "99")
   end
 end
